1c7b98e44b9e5bb4022f0450b869c6952000cd58
joelkbennett <joelkbennett@gmail.com>
2016-02-19 22:06:16 +0000

    Add new messages

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,30 @@
 get '/' do
   erb :index
 end
+
+get '/messages' do
+  @messages = Message.all
+  erb :'messages/index'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+  )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+end
+
+get '/messages/new' do
+  erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
